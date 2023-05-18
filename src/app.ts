270a8f3b2a04a8d5564ee49eeca4dea66952f0ff
dexgapuz <dxgapuz@gmail.com>
2023-05-19 02:29:09 +0800

    button events and screen numbers

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import './sass/styles.scss';
 import './styles/app.css';
 import 'bootstrap';
+import * as checkType from 'check-types';
+
+const numericButton: HTMLCollectionOf<Element> = document.getElementsByClassName('numeric-button')!;
+const screen: HTMLElement = document.querySelector('.screen')!;
+let inputs: string[] = [];
+let num: number = 0;
+
+Array.from(numericButton).forEach((element: Element) => {
+    element.addEventListener('click', (event: Event) => {
+        let button: HTMLButtonElement = event.target as HTMLButtonElement;
+        if (checkType.string(button.dataset.value)) {
+            setScreen(button.dataset.value)
+        }
+    });
+});
+
+function setScreen(value: string): void {
+    inputs.push(value);
+    const concantNumbers = inputs.join('');
+    screen.innerText = concantNumbers
+    num = parseInt(concantNumbers);
+}
